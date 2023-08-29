0bf1d2835b1ac288c1a9b3a5788bb38f0a323f32
sauravmore1404 <98641972+sauravmore1404@users.noreply.github.com>
2023-08-30 01:51:16 +0530

    1 file modified

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -2,32 +2,31 @@ let Sdata = [
   {
     id: 1,
     imgsrc:
-      "https://m.media-amazon.com/images/M/MV5BNDVkYjU0MzctMWRmZi00NTkxLTgwZWEtOWVhYjZlYjllYmU4XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_FMjpg_UX1000_.jpg",
-    link: "https://www.netflix.com/search?q=frindes",
+      'https://m.media-amazon.com/images/M/MV5BNDVkYjU0MzctMWRmZi00NTkxLTgwZWEtOWVhYjZlYjllYmU4XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_FMjpg_UX1000_.jpg',
+    link: 'https://www.netflix.com/search?q=frindes',
   },
   {
     id: 2,
     imgsrc:
-      "https://www.tvguide.com/a/img/catalog/provider/1/1/1-6482810627.jpg",
-    link: "https://www.netflix.com/search?q=big%20bang",
+      'https://www.tvguide.com/a/img/catalog/provider/1/1/1-6482810627.jpg',
+    link: 'https://www.netflix.com/search?q=big%20bang',
   },
   {
     id: 3,
     imgsrc:
-      "https://m.media-amazon.com/images/M/MV5BNDVkYjU0MzctMWRmZi00NTkxLTgwZWEtOWVhYjZlYjllYmU4XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_FMjpg_UX1000_.jpg",
-    link: "https://www.netflix.com/search?q=frindes",
+      'https://m.media-amazon.com/images/M/MV5BYWE3MDVkN2EtNjQ5MS00ZDQ4LTliNzYtMjc2YWMzMDEwMTA3XkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_.jpg',
+    link: 'https://www.netflix.com/title/81040344',
   },
   {
     id: 4,
-    imgsrc:
-      "https://www.tvguide.com/a/img/catalog/provider/1/1/1-6482810627.jpg",
-    link: "https://www.netflix.com/search?q=big%20bang",
+    imgsrc: 'https://flxt.tmsimg.com/assets/p17580215_b_v13_ac.jpg',
+    link: 'https://www.netflix.com/title/80189685?trackId=255824129&trackIdJaw=255824129&trackIdEpisode=255824129&trackIdTrailer=255824129&dpRightClick=1',
   },
   {
     id: 5,
     imgsrc:
-      "https://m.media-amazon.com/images/M/MV5BNDVkYjU0MzctMWRmZi00NTkxLTgwZWEtOWVhYjZlYjllYmU4XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_FMjpg_UX1000_.jpg",
-    link: "https://www.netflix.com/search?q=frindes",
+      'https://resizing.flixster.com/j3Pk-1QBwBZ7_pn6rGObBhi4eoY=/ems.cHJkLWVtcy1hc3NldHMvdHZzZWFzb24vUlRUVjMxNTc1OC53ZWJw',
+    link: 'https://www.netflix.com/title/80115328',
   },
 ];
 export default Sdata;
